d1081d4b28f0c64be78d5872434bd1a12f13af9d
Prabhat Awasthi <90645975+prabhat3697@users.noreply.github.com>
2026-03-15 13:19:00 +0530

    Update shibudb.rb

--- Formula/shibudb.rb
+++ Formula/shibudb.rb
@@ -3,7 +3,7 @@ class Shibudb < Formula
   homepage "https://github.com/shibudb-org/shibudb-server"
   url "https://github.com/shibudb-org/shibudb-server/releases/download/v1.0.2/shibudb-1.0.2-darwin-arm64.tar.gz"
   sha256 "a1940ebc28c8080d586ae03fee9435bdaf198063e9108ef510b0caa11b8895c3"
-  version "1.0.1"
+  version "1.0.2"
   license "AGPL3"
 
   def install
